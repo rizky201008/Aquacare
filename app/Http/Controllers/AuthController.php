90f0d259e161cdef11c27977b91bb5047350d1d3
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-04-03 20:16:36 +0700

    feat: create controller AuthController.php

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+
+class AuthController extends Controller
+{
+//    function for show login page
+    public function login()
+    {
+        return Inertia::render('Pages/Auth/Login');
+    }
+
+//    function for show register page
+    public function register()
+    {
+        return Inertia::render('Pages/Auth/Register');
+    }
+
+//    function for logout user
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+
+//    function for authenticate user
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
+    }
+
+//    function for save user
+    public function registerUser(Request $request)
+    {
+        $credentials = $request->validate([
+            'name' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        $user = User::create($credentials);
+
+        Auth::login($user);
+
+        return redirect()->intended('dashboard');
+    }
+}
