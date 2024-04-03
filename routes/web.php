6b45399ef69d6eeea2ae96a402b303de82890338
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-04-03 20:17:21 +0700

    refactor: create new route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login', [AuthController::class, 'login'])->name('login');
+Route::get('register', [AuthController::class, 'register'])->name('register');
+Route::post('login', [AuthController::class, 'authenticate'])->name('login.post');
+Route::post('register', [AuthController::class, 'registerUser'])->name('register.post');
+Route::get('dashboard', function () {
+    return "Hello";
+})->name('dashboard');
