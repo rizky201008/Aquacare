6f6f6349c6d34c23ab14488509dc4e4205159cbf
Haerul Taka <haerultaka255@gmail.com>
2024-04-16 19:47:17 +0700

    create dashboard admin

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,9 @@ Route::get('/', function () {
 
 Route::get('login', [AuthController::class, 'login'])->name('login');
 Route::get('register', [AuthController::class, 'register'])->name('register');
+Route::get('dashboard', [AuthController::class, 'dashboard'])->name('dashboard');
 Route::post('login', [AuthController::class, 'authenticate'])->name('login.post');
 Route::post('register', [AuthController::class, 'registerUser'])->name('register.post');
-Route::get('dashboard', function () {
-    return "Hello";
-})->name('dashboard');
+// Route::get('dashboard', function () {
+//     return "Hello";
+// })->name('dashboard');
