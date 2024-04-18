a7ba8c1aad5cf2d796a36da9433dad67e3af71a7
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-04-18 15:40:05 +0700

    refactor: add conditional rendering

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,24 +1,55 @@
 import AdminLayout from "@/Layouts/AdminLayout";
 // import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from "@inertiajs/react";
+import {Head} from "@inertiajs/react";
 import "../../css/sb-admin-2.css";
 
-export default function Dashboard({ auth }) {
+export default function Dashboard({auth}) {
+    let roleContent = null;
+
+    switch (auth.user.roles.name) {
+        case 'user':
+            roleContent = (
+                <div>
+                    <h2>Welcome, Role User!</h2>
+                    {/* Tambahkan konten khusus untuk role A di sini */}
+                </div>
+            );
+            break;
+        case 'admin':
+            roleContent = (
+                <div>
+                    <h2>Welcome, Role Admin!</h2>
+                    {/* Tambahkan konten khusus untuk role B di sini */}
+                </div>
+            );
+            break;
+        case 'petugas':
+            roleContent = (
+                <div>
+                    <h2>Welcome, Role Petugas!</h2>
+                    {/* Tambahkan konten khusus untuk role C di sini */}
+                </div>
+            );
+            break;
+        default:
+            roleContent = (
+                <div>
+                    <h2>Unauthorized Access</h2>
+                    <p>You don't have permission to access this page.</p>
+                </div>
+            );
+    }
     return (
         <AdminLayout
             header={
                 <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-500 leading-tight"></h2>
             }
         >
-            <Head title="Dashboard" />
+            <Head title="Dashboard"/>
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    {/* <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">
-                            You're logged in!
-                        </div>
-                    </div> */}
+                    {roleContent}
                 </div>
             </div>
         </AdminLayout>
