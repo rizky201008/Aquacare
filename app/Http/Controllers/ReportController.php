0f04a23e0e7053e0b409ed28fde57558ab8aabe3
Haerul Taka <haerultaka255@gmail.com>
2024-04-23 11:30:21 +0700

    post untuk laporan

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -12,7 +12,7 @@ class ReportController extends Controller
     public function reportList()
     {
         return Inertia::render('Report', [
-            'report' => Report::latest()->get()
+            'reports' => Report::latest()->get()
         ]);
     }
 
@@ -35,25 +35,11 @@ class ReportController extends Controller
     public function feedbackList()
     {
         return Inertia::render('Feedback', [
-            'feedback' => Feedback::latest()->get()
+            'feedbacks' => Feedback::latest()->get()
         ]);
     }
 
-    private function createReport(array $data)
-    {
-        try {
-            Report::create([
-                'rasa' => $data['rasa'],
-                'suhu' => $data['suhu'],
-                'kekentalan' => $data['kekentalan'],
-                'detail' => $data['detail'],
-                'status' => $data['status'] ?? 'pending',
-                'user_id' => $data['user_id']
-            ]);
-        } catch (\Exception $e) {
-            throw new $e->getMessage();
-        }
-    }
+    
 
     private function createReport(array $data)
     {
--- resources/js/Pages/Report.jsx
+++ resources/js/Pages/Report.jsx
@@ -1,13 +1,28 @@
 import AdminLayout from "@/Layouts/AdminLayout";
 import React, { useState } from "react";
-import "../script";
+
 import { Head, Link, router, useForm, usePage } from "@inertiajs/react";
 import { Flowbite } from "flowbite-react";
 import Popup from "reactjs-popup";
 
 export default function Report() {
-    const { data, users, reports } = usePage().props;
+    const { flash, errors, reports } = usePage().props;
     const [showModal, setShowModal] = useState(false);
+    const { data, setData, reset } = useForm({
+        rasa: "",
+        suhu: "",
+        kekentalan: "",
+        detail: "",
+        user_id: "",
+    });
+    const storeReport = (e) => {
+        e.preventDefault();
+        router.post("/report", data, {
+            onSuccess: () => {
+                reset();
+            },
+        });
+    };
 
     const openModal = () => {
         setShowModal(true);
@@ -25,87 +40,140 @@ export default function Report() {
                         <h3 className="font-semibold text-base text-blueGray-700">
                             Daftar Laporan
                         </h3>
+                        {flash.message && (
+                            <div
+                                className="flex items-center p-4 mb-4 text-sm text-midnight rounded-lg  dark:text-green-500"
+                                role="alert"
+                            >
+                                <svg
+                                    className="flex-shrink-0 inline w-4 h-4 me-3"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 20 20"
+                                >
+                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                                </svg>
+                                <span className="sr-only">Info</span>
+                                <div>
+                                    <span className="font-medium">
+                                        Success alert!
+                                    </span>{" "}
+                                    {flash.message}
+                                </div>
+                            </div>
+                        )}
                     </div>
-                    
+
                     <div className="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
                         <Popup
                             modal
-                            trigger={<button className="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">Create</button>}
-                            
+                            trigger={
+                                <button className="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
+                                    Create
+                                </button>
+                            }
                         >
-                            <form className="p-4 md:p-5 bg-slate-400">
+                            <form
+                                className="p-4 md:p-5 bg-slate-400 "
+                                onSubmit={storeReport}
+                            >
                                 <div className="grid gap-4 mb-4 grid-cols-2">
                                     <div className="col-span-2">
                                         <label
                                             htmlFor="name"
                                             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                                         >
-                                            Name
+                                            Rasa
                                         </label>
                                         <input
                                             type="text"
-                                            name="name"
-                                            id="name"
-                                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                            placeholder="Type product name"
-                                            required=""
+                                            // id="default-search"
+                                            className="bg-sea border text-midnight border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                                            placeholder="enter ..."
+                                            // required
+                                            onChange={(e) =>
+                                                setData("rasa", e.target.value)
+                                            }
+                                            value={data.rasa}
                                         />
-                                    </div>
-                                    <div className="col-span-2 sm:col-span-1">
-                                        <label
-                                            htmlFor="price"
-                                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                                        >
-                                            Price
-                                        </label>
-                                        <input
-                                            type="number"
-                                            name="price"
-                                            id="price"
-                                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                            placeholder="$2999"
-                                            required=""
-                                        />
-                                    </div>
-                                    <div className="col-span-2 sm:col-span-1">
-                                        <label
-                                            htmlFor="category"
-                                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                                        >
-                                            Category
-                                        </label>
-                                        <select
-                                            id="category"
-                                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                        >
-                                            <option selected="">
-                                                Select category
-                                            </option>
-                                            <option value="TV">
-                                                TV/Monitors
-                                            </option>
-                                            <option value="PC">PC</option>
-                                            <option value="GA">
-                                                Gaming/Console
-                                            </option>
-                                            <option value="PH">Phones</option>
-                                        </select>
+                                        <p className="text-red-500 text-sm mt-2">
+                                    {errors.rasa}
+                                </p>
                                     </div>
                                     <div className="col-span-2">
                                         <label
-                                            htmlFor="description"
+                                            htmlFor="name"
                                             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                                         >
-                                            Product Description
+                                            Suhu
                                         </label>
-                                        <textarea
-                                            id="description"
-                                            rows={4}
-                                            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                            placeholder="Write product description here"
-                                            defaultValue={""}
+                                        <input
+                                            type="text"
+                                            // id="default-search"
+                                            className="bg-sea border text-midnight border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                                            placeholder="enter ..."
+                                            // required
+                                            onChange={(e) =>
+                                                setData("suhu", e.target.value)
+                                            }
+                                            value={data.suhu}
                                         />
+                                        <p className="text-red-500 text-sm mt-2">
+                                            {errors.suhu}
+                                        </p>
                                     </div>
+                                    <div className="col-span-2">
+                                        <label
+                                            htmlFor="name"
+                                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                        >
+                                            Kekentalan
+                                        </label>
+                                        <input
+                                            type="text"
+                                            // id="default-search"
+                                            className="bg-sea border text-midnight border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                                            placeholder="enter ..."
+                                            // required
+                                            onChange={(e) =>
+                                                setData(
+                                                    "kekentalan",
+                                                    e.target.value
+                                                )
+                                            }
+                                            value={data.kekentalan}
+                                        />
+                                        <p className="text-red-500 text-sm mt-2">
+                                            {errors.kekentalan}
+                                        </p>
+                                    </div>
+                                    <div className="col-span-4">
+                                        <label
+                                            htmlFor="name"
+                                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                        >
+                                            Detail Air
+                                        </label>
+                                        <input
+                                            type="text"
+                                            // id="default-search"
+                                            className="bg-sea border text-midnight border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                                            placeholder="enter ..."
+                                            // required
+                                            onChange={(e) =>
+                                                setData(
+                                                    "detail",
+                                                    e.target.value
+                                                )
+                                            }
+                                            value={data.detail}
+                                        />
+                                        <p className="text-red-500 text-sm mt-2">
+                                            {errors.detail}
+                                        </p>
+                                    </div>
+                            
                                 </div>
                                 <button
                                     type="submit"
@@ -123,16 +191,9 @@ export default function Report() {
                                             clipRule="evenodd"
                                         />
                                     </svg>
-                                    Add new product
+                                    Add New Report
                                 </button>
-                                <button
-                                    onClick={closeModal}
-                                    type="submit"
-                                    className="text-white inline-flex items-center bg-black hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center m-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                                >
-                                    <svg className="me-1 -ms-1 w-5 h-5"></svg>
-                                    Tutup
-                                </button>
+                                
                             </form>
                         </Popup>
                         {/* <button
@@ -311,26 +372,7 @@ export default function Report() {
 
                                     <td className="px-6 py-4">
                                         <div className="flex justify-end gap-4">
-                                            <a
-                                                x-data="{ tooltip: 'Delete' }"
-                                                href="#"
-                                            >
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    fill="none"
-                                                    viewBox="0 0 24 24"
-                                                    strokeWidth="1.5"
-                                                    stroke="currentColor"
-                                                    className="h-6 w-6"
-                                                    x-tooltip="tooltip"
-                                                >
-                                                    <path
-                                                        strokeLinecap="round"
-                                                        strokeLinejoin="round"
-                                                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                                                    />
-                                                </svg>
-                                            </a>
+                                            
                                             <a
                                                 x-data="{ tooltip: 'Edite' }"
                                                 href="#"
