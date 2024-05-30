66e743739a38e98c041ca2edd5beb3752bab9ac5
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-05-30 13:22:13 +0700

    modify BlogItem.jsx

--- resources/js/Components/BlogItem.jsx
+++ resources/js/Components/BlogItem.jsx
@@ -1,17 +1,17 @@
-
 const BlogItem = ({blogItem, buttons}) => {
     return (
         <div className="card card-compact w-96 bg-white shadow-xl">
             <figure>
                 <img
                     src={blogItem.image_url}
-                    alt={''}/>
+                    alt={''}
+                className='h-52 w-full'/>
             </figure>
             <div className="card-body">
-                <h2 className="card-title">
+                <h2 className="card-title text-start w-full overflow-hidden truncate">
                     {blogItem.title}
                 </h2>
-                <p className='w-full text-start'>{blogItem.content}</p>
+                <p className="w-full text-start overflow-hidden truncate">{blogItem.content}</p>
                 <div className="card-actions justify-end">
                     {buttons}
                 </div>
