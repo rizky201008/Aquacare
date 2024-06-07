f8c593b41a7c394bf068acde2285d3634626890e
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-06-07 22:31:54 +0700

    replace h2 into arrow

--- resources/js/Components/BlogItem.jsx
+++ resources/js/Components/BlogItem.jsx
@@ -1,6 +1,6 @@
-const BlogItem = ({ blogItem, buttons }) => {
+const BlogItem = ({blogItem, buttons}) => {
     return (
-        <div className="card card-compact bg-white shadow-xl h-80">
+        <div className="card card-compact bg-white shadow-xl">
             <figure>
                 <img
                     src={blogItem.image_url}
@@ -8,10 +8,11 @@ const BlogItem = ({ blogItem, buttons }) => {
                     className='h-52 w-full'
                 />
             </figure>
-            <div className="card-body">
-                <h2 className="card-title text-start w-full flex flex-wrap overflow-hidden text-gray-950">
+            <div className="card-body h-52">
+                <a href={route('blogs.show', {slug: blogItem.slug})}
+                   className="card-title text-start w-full text-gray-950">
                     {blogItem.title}
-                </h2>
+                </a>
                 <p className="w-full text-start overflow-hidden truncate-lines">{blogItem.content}</p>
                 <div className="card-actions justify-end">
                     {buttons}
